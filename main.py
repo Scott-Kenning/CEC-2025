269ba66f3cc0d5763ae24999a49ee7941cef28d8
Christian Bookout <christianmbookout@gmail.com>
2025-03-15 15:53:05 -0300

    attempted api fix

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
-import os
-import tempfile
+import base64
+from io import BytesIO
+import cv2
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from matplotlib import pyplot as plt
 from predict import predict_image
+from PIL import Image
 
 app = FastAPI()
 
 @app.post("/classify")
 async def classify(file: UploadFile = File(...)):
-    if file.content_type not in ["image/jpeg", "image/jpg", "image/png"]:
-        return {"error": "Invalid file type"}
-
     image_data = await file.read()
+    original_image = Image.open(BytesIO(image_data)).convert("RGB")
+    prediction, predicted_prob, heat_map = predict_image(original_image)
+    
+    plt.figure(figsize=(8, 8))
+    plt.imshow(cv2.cvtColor(heat_map, cv2.COLOR_BGR2RGB))
+    plt.axis('off')
+    plt.show()
 
-    print("test", flush=True)
-
-    # Create a temporary file with an appropriate suffix
-    suffix = ".jpg" if file.content_type == "image/jpeg" else ".png"
-    with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
-        tmp.write(image_data)
-        tmp_path = tmp.name
-
-    try:
-        # Call predict_image with the temporary file path
-        prediction, predicted_prob, heat_map = predict_image(tmp_path)
-    finally:
-        # Clean up the temporary file
-        os.remove(tmp_path)
-
-    return {
+    result = {
         "classification": "Positive" if prediction == 1 else "Negative",
-        "confidence": (1 - predicted_prob) if prediction == 0 else predicted_prob
+        "confidence": predicted_prob if prediction == 1 else (1 - predicted_prob),
+        "image": base64.b64encode(heat_map).decode('utf-8')
     }
+
+    return JSONResponse(content=result)
