8721cf9d7c42eee0edcfd4e1d6d0213c543ce1e4
Julian France <julian123france@gmail.com>
2025-03-15 13:40:52 -0300

    classify endpoint v1

---
+++ backend.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, File, UploadFile
+#import base64
+from predict import *
+
+app = FastAPI()
+
+@app.post("/classify")
+async def classify(image_file: UploadFile = File(...)):
+
+    if (image_file.content_type != "image/jpeg" and image_file.content_type != "image/png"):
+        return {
+            "error": "Invalid file type"
+        }
+
+    prediction, predicted_prob = predict_image(image_file.filename)
+    
+    # Convert the processed image to a Base64-encoded string
+    # buffered = io.BytesIO()
+    # processed_image.save(buffered, format="PNG")
+    # processed_image_base64 = base64.b64encode(buffered.getvalue()).decode("utf-8")
+    
+    return {
+        "decision": prediction,
+        "confidence": predicted_prob
+    }
+
+    
